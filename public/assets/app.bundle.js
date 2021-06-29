10ea2ed8235a9e6616d17bb04db06c76b4b64d73
Federico Santana <fede.santana@hotmail.com>
2021-06-29 11:53:35 -0300

    add webpack 5

--- public/assets/app.bundle.js
+++ public/assets/app.bundle.js
@@ -15,7 +15,7 @@
   \****************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("console.log('App loader');\n\nvar config = __webpack_require__(/*! ./config.json */ \"./config.json\");\n\nconsole.log(config);\nconsole.log(config['title']);\n\n//# sourceURL=webpack://git/./app.js?");
+eval("console.log('App loader');\n\nvar config = __webpack_require__(/*! ./config.json */ \"./config.json\");\nvar ejemplo1 = __webpack_require__(/*! ./Test.p5js */ \"./Test.p5js\");\n\nconsole.log(config);\nconsole.log(ejemplo1);\n\n//# sourceURL=webpack://git/./app.js?");
 
 /***/ }),
 
@@ -27,6 +27,16 @@ eval("console.log('App loader');\n\nvar config = __webpack_require__(/*! ./confi
 
 "use strict";
 eval("module.exports = JSON.parse('{\"title\":\"App title here\",\"version\":\"1.0.1\"}');\n\n//# sourceURL=webpack://git/./config.json?");
+
+/***/ }),
+
+/***/ "./Test.p5js":
+/*!*******************!*\
+  !*** ./Test.p5js ***!
+  \*******************/
+/***/ (() => {
+
+eval("function setup() {\n  createCanvas(400, 400);\n}\n\nfunction draw() {\n  background(220);\n}\n\n//# sourceURL=webpack://git/./Test.p5js?");
 
 /***/ })
 
